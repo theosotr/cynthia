7728d0bbd1b506ad4f66dc97c3f291398c001d27
Thodoris Sotiropoulos <theosotr@aueb.gr>
2020-03-11 12:57:27 +0200

    Make schema compatible to sqlite3

--- bugs/listing/schema.sql
+++ bugs/listing/schema.sql
@@ -1,4 +1,4 @@
-drop table listings;
+drop table if exists listings;
 create table if not exists listings (
   id serial not null primary key,
   sale_price numeric (10) not null,
@@ -6,9 +6,9 @@ create table if not exists listings (
   toto boolean not null
 );
 
-insert into listings (sale_price, yearly_rent, toto)
-values (30.4, 12.1, false);
-insert into listings (sale_price, yearly_rent, toto)
-values (12.0, 242.11, false);
-insert into listings (sale_price, yearly_rent, toto)
-values (195.12, 3.3, true);
+insert into listings (id, sale_price, yearly_rent, toto)
+  values (1, 30.4, 12.1, '0');
+insert into listings (id, sale_price, yearly_rent, toto)
+  values (2, 12.0, 242.11, '0');
+insert into listings (id, sale_price, yearly_rent, toto)
+  values (3, 195.12, 3.3, '1');
