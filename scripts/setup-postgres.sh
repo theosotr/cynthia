85f31439af05f2ffdb6fac3aaef88a14d95ae4c2
Thodoris Sotiropoulos <theosotr@aueb.gr>
2020-03-11 11:15:46 +0200

    Add script for setting up postgres

---
+++ scripts/setup-postgres.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+sudo apt update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install postgresql postgresql-contrib -yq
+sudo -u postgres /usr/lib/postgresql/10/bin/pg_ctl \
+  -D /etc/postgresql/10/main/ -l /var/lib/postgresql/logfile start
+sudo -u postgres createuser orm_testing
+sudo -u postgres psql -c "alter user orm_testing with encrypted password 'orm_testing';"
+ sudo -u postgres psql -c "alter user orm_testing with superuser;"
