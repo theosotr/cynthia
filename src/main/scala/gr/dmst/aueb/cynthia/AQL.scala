520552852034e8c33fa560a4f51a4d32c5953596
Thodoris Sotiropoulos <theosotr@aueb.gr>
2020-03-13 17:11:48 +0200

    First steps on AQL language

---
+++ src/main/scala/gr/dmst/aueb/cynthia/AQL.scala
@@ -0,0 +1,14 @@
+package gr.dmst.aueb.cynthia
+
+
+sealed trait Predicate
+case class Eq(key: String, value: String) extends Predicate
+
+sealed trait Operation
+case class Filter (pred: Predicate) extends Operation
+
+sealed trait Query
+case class Getall(model: String, fields: Option[Array[String]]) extends Query
+case class Apply(op: Operation, q: Query)
+case class Union(q1: Query, q2: Query)
+case class Intersect(q1: Query, q2: Query)
