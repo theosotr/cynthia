c67d90388a1b4c26637f8bee0b3b25d33373b8da
Thodoris Sotiropoulos <theosotr@aueb.gr>
2020-03-16 11:11:12 +0200

    Query => QuerySet

--- src/main/scala/gr/dmst/aueb/cynthia/AQL.scala
+++ src/main/scala/gr/dmst/aueb/cynthia/AQL.scala
@@ -7,8 +7,8 @@ case class Eq(key: String, value: String) extends Predicate
 sealed trait Operation
 case class Filter (pred: Predicate) extends Operation
 
-sealed trait Query
-case class Getall(model: String, fields: Option[Array[String]]) extends Query
-case class Apply(op: Operation, q: Query) extends Query
-case class Union(q1: Query, q2: Query) extends Query
-case class Intersect(q1: Query, q2: Query) extends Query
+sealed trait QuerySet
+case class New(model: String, fields: Option[Array[String]]) extends QuerySet
+case class Apply(op: Operation, q: QuerySet) extends QuerySet
+case class Union(q1: QuerySet, q2: QuerySet) extends QuerySet
+case class Intersect(q1: QuerySet, q2: QuerySet) extends QuerySet
