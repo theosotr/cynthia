65964957ccf72f9e77d115a247e6ba8c0748fb25
Thodoris Sotiropoulos <theosotr@aueb.gr>
2020-03-17 11:47:54 +0200

    Enrich CLI interface of Cynthia

--- src/main/scala/gr/dmst/aueb/cynthia/Cynthia.scala
+++ src/main/scala/gr/dmst/aueb/cynthia/Cynthia.scala
@@ -6,7 +6,9 @@ import gr.dmst.aueb.cynthia.Utils
 
 
 case class Options (
-  schemas: String = ""
+  schemas: String = "",
+  orms: Seq[String] = Seq(),
+  dbs: Seq[String] = Seq()
 )
 
 
@@ -48,7 +50,32 @@ object Cynthia {
         opt[String]('s', "schemas")
           .required()
           .action((x, o) => o.copy(schemas = x))
-          .text("Path to database schemas")
+          .text("Path to database schemas"),
+        opt[Seq[String]]('o', "orms")
+          .required()
+          .action((x, o) => o.copy(orms = x))
+          .validate(_.foldLeft (success) { (acc, x) => x match {
+              case "django"     => acc
+              case "sqlalchemy" => acc
+              case _            => failure("ORM '" + x + "' is not supported")
+            }
+          })
+          .text("ORMs to differentially test"),
+        opt[Seq[String]]('d', "backends")
+          .action((x, o) => o.copy(dbs = x))
+          .validate(_.foldLeft (success) { (acc, x) => x match {
+              case "mysql" | "postgres" => acc
+              case "sqlite"             => failure ("SQLite is used by default")
+              case _                    => failure ("Database backend '" + x + "' is not supported")
+            }
+          })
+          .text("Database backends to store data"),
+        checkConfig(x =>
+          if (x.dbs.length + x.orms.length < 2)
+            failure(
+              "Number of database backends + number of ORMs must be greather than 1")
+          else success
+        )
       )
     }
 
