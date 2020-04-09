c3459445859d6e4ccc110f33e942f0da04a9190d
Thodoris Sotiropoulos <theosotr@aueb.gr>
2020-04-09 15:09:58 +0300

    Fix SQL scripts to be compatible with Postgres

--- examples/books.sql
+++ examples/books.sql
@@ -1,3 +1,5 @@
+drop table if exists review;
+drop table if exists book;
 drop table if exists author;
 CREATE TABLE author (
       id serial primary key,
@@ -5,7 +7,6 @@ CREATE TABLE author (
       surname varchar(50) NOT NULL
 );
 
-drop table if exists book;
 CREATE TABLE book (
       id serial primary key,
       title varchar(100) NOT NULL,
@@ -15,7 +16,6 @@ CREATE TABLE book (
       FOREIGN KEY (author_id) REFERENCES author(id) ON DELETE CASCADE
 );
 
-drop table if exists review;
 CREATE TABLE review (
       id serial primary key,
       book_id integer NOT NULL,
@@ -27,42 +27,42 @@ CREATE TABLE review (
 
 
 insert into author(id, first_name, surname)
-  values(1, "Joe", "Coecker");
+  values(1, 'Joe', 'Coecker');
 insert into author(id, first_name, surname)
-  values(2, "Foel", "Jessie");
+  values(2, 'Foel', 'Jessie');
 insert into author(id, first_name, surname)
-  values(3, "James", "Brown");
+  values(3, 'James', 'Brown');
 
 insert into book(id, title, author_id, isbn)
-  values (1, "Programming in C", 1, "12345678-012");
+  values (1, 'Programming in C', 1, '12345678-012');
 insert into book(id, title, author_id, isbn)
-  values (2, "Random book", 1, "12345679-012");
+  values (2, 'Random book', 1, '12345679-012');
 insert into book(id, title, author_id, isbn)
-  values (3, "ABC Inc.", 2, "12345678-014");
+  values (3, 'ABC Inc.', 2, '12345678-014');
 insert into book(id, title, author_id, isbn)
-  values (4, "travel world", 3, "12345678-015");
+  values (4, 'travel world', 3, '12345678-015');
 insert into book(id, title, author_id, isbn)
-  values (5, "What else?", 3, "12345678-016");
+  values (5, 'What else?', 3, '12345678-016');
 insert into book(id, title, author_id, isbn)
-  values (6, "ABCD_F", 3, "12345678-017");
+  values (6, 'ABCD_F', 3, '12345678-017');
 
 insert into review(id, book_id, reviewer_name, content, rating)
-  values(1, 1, "Reviewer A", "Super", 5);
+  values(1, 1, 'Reviewer A', 'Super', 5);
 insert into review(id, book_id, reviewer_name, content, rating)
-  values(2, 1, "Apollo12", "Fairly Good", 4);
+  values(2, 1, 'Apollo12', 'Fairly Good', 4);
 insert into review(id, book_id, reviewer_name, content, rating)
-  values(3, 2, "Reviewer A", "Moderate", 3);
+  values(3, 2, 'Reviewer A', 'Moderate', 3);
 insert into review(id, book_id, reviewer_name, content, rating)
-  values(5, 2, "Kine Shot", "Decent", 3);
+  values(5, 2, 'Kine Shot', 'Decent', 3);
 insert into review(id, book_id, reviewer_name, content, rating)
-  values(6, 2, "Jessica Alba", "Not good", 1);
+  values(6, 2, 'Jessica Alba', 'Not good', 1);
 insert into review(id, book_id, reviewer_name, content, rating)
-  values(7, 3, "Reviewer A", "Dissapointing", 1);
+  values(7, 3, 'Reviewer A', 'Dissapointing', 1);
 insert into review(id, book_id, reviewer_name, content, rating)
-  values(8, 3, "Jessica Alba", "Excellent", 5);
+  values(8, 3, 'Jessica Alba', 'Excellent', 5);
 insert into review(id, book_id, reviewer_name, content, rating)
-  values(9, 4, "random rev", "Blah", 2);
+  values(9, 4, 'random rev', 'Blah', 2);
 insert into review(id, book_id, reviewer_name, content, rating)
-  values(10, 6, "random rev", "Good", 4);
+  values(10, 6, 'random rev', 'Good', 4);
 insert into review(id, book_id, reviewer_name, content, rating)
-  values(11, 6, "Reviewer A", "Briliant", 5);
+  values(11, 6, 'Reviewer A', 'Briliant', 5);
--- examples/listing.sql
+++ examples/listing.sql
@@ -10,6 +10,6 @@ create table if not exists listing (
 insert into listing (id, sale_price, yearly_rent, toto, foo)
   values (1, 30.4, 12.1, '0', 'bar');
 insert into listing (id, sale_price, yearly_rent, toto, foo)
-  values (2, 12.0, 242.11, '0', 'baz');
+  values (2, 30.4, 242.11, '0', 'baz');
 insert into listing (id, sale_price, yearly_rent, toto, foo)
   values (3, 195.12, 3.3, '1', 'qux');
