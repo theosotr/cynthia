5e3507679a60c51816ea2d93b2d4dd54a1de447c
Thodoris Sotiropoulos <theosotr@aueb.gr>
2020-04-09 12:43:58 +0300

    Sort results if queryset is not ordered

--- src/main/scala/gr/dmst/aueb/cynthia/QueryExecutor.scala
+++ src/main/scala/gr/dmst/aueb/cynthia/QueryExecutor.scala
@@ -25,16 +25,22 @@ object QueryExecutor {
   
   def apply(q: Query, target: Target) = {
     Try(ORMTranslator(q, target)) match {
-      case Success(q) =>
-        Utils.writeToFile(target.orm.getDriverPath(target.db), q)
+      case Success(ormQ) =>
+        Utils.writeToFile(target.orm.getDriverPath(target.db), ormQ)
         val (stdout, stderr) = (new StringBuilder, new StringBuilder)
         target.getTargetCommand() !
         ProcessLogger(stdout append _ + "\n", stderr append _ + "\n") match {
           case 0 =>
             if (stderr.length != 0 && stdout.length == 0)
               Fail(stderr.toString)
-            else
-              Ok(stdout.toString)
+            else 
+              if (q.queryset.ordered) Ok(stdout.toString)
+              else {
+                // If the queryset is not ordered we sort the results
+                // so that we can perform safe comparisons about the output.
+                val l = stdout.toString.split('\n').sortWith(_ < _)
+                Ok(l mkString "\n")
+              }
           case _ => Fail(stderr.toString)
         }
       case Failure(UnsupportedException(msg)) => Unsupported(msg)
