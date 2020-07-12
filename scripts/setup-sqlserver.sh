5b7a10c57fca37c34de97749bb1e7c50b021f94b
Thodoris Sotiropoulos <theosotr@aueb.gr>
2020-07-12 10:54:38 +0300

    update script for setting up MSSQL

--- scripts/setup-sqlserver.sh
+++ scripts/setup-sqlserver.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add - && \
 sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/18.04/mssql-server-2019.list)" && \
@@ -11,8 +11,7 @@ curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list | sudo tee /et
 sudo apt-get update && \
 sudo apt-get install mssql-tools unixodbc-dev && \
 sudo apt-get -y install libodbc1 unixodbc unixodbc-dev freetds-dev freetds-bin tdsodbc && \
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile && \
-source ~/.bashrc && \
-sqlcmd -S localhost -U orm_testing -P 'orm_testing1' -Q "CREATE LOGIN orm_testing WITH PASSWORD = 'orm_testing1'" && \
-sqlcmd -S localhost -U orm_testing -P 'orm_testing1' -Q "EXEC sp_addsrvrolemember 'orm_testing', 'sysadmin'" && \
-# sqlcmd -S localhost -U orm_testing -P 'orm_testing1'
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.zshrc && \
+sqlcmd -S localhost -U SA -P 'orm_testing1' -Q "CREATE LOGIN orm_testing WITH PASSWORD = 'orm_testing1'" && \
+sqlcmd -S localhost -U SA -P 'orm_testing1' -Q "EXEC sp_addsrvrolemember 'orm_testing', 'sysadmin'"
+#sqlcmd -S localhost -U orm_testing -P 'orm_testing1'
