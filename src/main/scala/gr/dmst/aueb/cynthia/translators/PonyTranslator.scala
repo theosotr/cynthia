a70a30503ecfa92481c25de8b84866d5b540ddeb
StefanosChaliasos <stefanoshaliassos@gmail.com>
2020-07-29 12:48:48 +0300

    pony: Add support for first query from Books
    * Execute and print the results of a query
    * Handle simple order_by

--- src/main/scala/gr/dmst/aueb/cynthia/translators/PonyTranslator.scala
+++ src/main/scala/gr/dmst/aueb/cynthia/translators/PonyTranslator.scala
@@ -32,12 +32,69 @@ case class PonyTranslator(target: Target) extends Translator {
     |                print(label, x if x is not None else '0.00')
     |""".stripMargin
 
-  override def emitPrint(q: Query, dFields: Seq[String], ret: String) = ""
 
-  override def constructCombinedQuery(s: State) = QueryStr(Some("var"))
+  def getPonyFieldName(field: String) =
+    field.split('.').toList match {
+      case Nil | _ :: Nil => field
+      case _ :: t         => t mkString "__"
+    }
+
+  override def emitPrint(q: Query, dFields: Seq[String], ret: String) = {
+    def _dumpField(v: String, fields: Iterable[String], ident: String = "") =
+      fields map { as => s"${ident}dump(getattr($v, '$as', None), '$as')" } mkString "\n"
+
+    val toPrint = q match {
+      case SetRes(_) | SubsetRes(_, _, _) =>
+        s"    for r in $ret:\n${_dumpField("r", dFields, ident = " " * 8)}"
+      case _ => ???
+    }
+    "with db_session:\n" + toPrint
+  }
+
+  def constructQueryPrefix(s: State) =  s.query match {
+    case None    =>
+      val qstr = s"select(i for i in ${s.source})"
+      QueryStr(
+        Some("ret" + s.numGen.next().toString),
+        Some(qstr)
+      )
+    case Some(q) => q
+  }
+
+  def constructOrderBy(s: State) = s.orders match {
+    case Seq() => ""
+    case spec  => {
+      (
+        Str("order_by(lambda i: (") << (
+          spec map { x =>
+            x match {
+              case (k, Desc) => "desc(i." + getPonyFieldName(k) + ")"
+              case (k, Asc)  => "i." + getPonyFieldName(k)
+            }
+          } mkString ","
+        ) << "))"
+      ).!
+      "order_by(lambda i: (desc(i.rating), desc(i.id)))"
+    }
+  }
+
+  override def constructCombinedQuery(s: State) = QueryStr(Some("combined"))
 
   override def constructNaiveQuery(s: State, first: Boolean, offset: Int,
-      limit: Option[Int]) = QueryStr(Some("var"))
+      limit: Option[Int]) = {
+    val qStr = constructQueryPrefix(s)
+    qStr >> QueryStr(Some("ret" + s.numGen.next().toString),
+      Some(
+        Seq(
+          qStr.ret.get,
+          constructOrderBy(s)
+        ) filter {
+          case "" => false
+          case _  => true
+        }  mkString(".")
+      )
+    )
+  }
 
   override def unionQueries(s1: State, s2: State) =
     throw new UnsupportedException("unions are not implemented")
