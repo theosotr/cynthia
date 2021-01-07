39024e913fb44214b7cea134965f2a8fac20740b
Thodoris Sotiropoulos <theosotr@aueb.gr>
2021-01-07 12:10:48 +0200

    Add LICENSE

--- src/main/scala/gr/dmst/aueb/cynthia/Words.scala
+++ src/main/scala/gr/dmst/aueb/cynthia/Words.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020-2021 Thodoris Sotiropoulos, Stefanos Chaliasos
+ *
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package gr.dmst.aueb.cynthia
 
 import scala.io.Source
