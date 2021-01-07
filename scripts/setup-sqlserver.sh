759b024729cf16adc9c0809e2756dd7ee299ec6d
Thodoris Sotiropoulos <theosotr@aueb.gr>
2021-01-07 15:52:32 +0200

    Update script for setting up MSSQL

--- scripts/setup-sqlserver.sh
+++ scripts/setup-sqlserver.sh
@@ -1,17 +1,27 @@
 #! /bin/bash
 
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add - && \
-sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/18.04/mssql-server-2019.list)" && \
-sudo apt update && \
-sudo apt-get install -y mssql-server && \
-sudo /opt/mssql/bin/mssql-conf setup && \
-systemctl status mssql-server --no-pager && \
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add - && \
-curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list && \
-sudo apt-get update && \
-sudo apt-get install mssql-tools unixodbc-dev && \
-sudo apt-get -y install libodbc1 unixodbc unixodbc-dev freetds-dev freetds-bin tdsodbc && \
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.zshrc && \
-sqlcmd -S localhost -U SA -P 'orm_testing1' -Q "CREATE LOGIN orm_testing WITH PASSWORD = 'orm_testing1'" && \
-sqlcmd -S localhost -U SA -P 'orm_testing1' -Q "EXEC sp_addsrvrolemember 'orm_testing', 'sysadmin'"
-#sqlcmd -S localhost -U orm_testing -P 'orm_testing1'
+MSSQL_SA_PASSWORD='orm_testing1'
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc |
+sudo apt-key add -
+sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/18.04/mssql-server-2019.list)"
+sudo apt update
+sudo apt-get install -y mssql-server
+sudo MSSQL_SA_PASSWORD=$MSSQL_SA_PASSWORD /opt/mssql/bin/mssql-conf -n setup accept-eula
+systemctl status mssql-server --no-pager
+
+curl https://packages.microsoft.com/keys/microsoft.asc |
+sudo apt-key add -
+
+curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list |
+sudo tee /etc/apt/sources.list.d/msprod.list
+
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y mssql-tools unixodbc-dev && \
+sudo apt-get -y install libodbc1 unixodbc unixodbc-dev freetds-dev freetds-bin tdsodbc
+
+export PATH="$PATH:/opt/mssql-tools/bin/"
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.zshrc
+
+sqlcmd -S localhost -U SA -P "$MSSQL_SA_PASSWORD" -Q "CREATE LOGIN orm_testing WITH PASSWORD = '$MSSQL_SA_PASSWORD'"
+sqlcmd -S localhost -U SA -P "$MSSQL_SA_PASSWORD" -Q "EXEC sp_addsrvrolemember 'orm_testing', 'sysadmin'"
