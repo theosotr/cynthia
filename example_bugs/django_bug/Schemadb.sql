069224ef17c349eda5d678fd0c6e3dd7baa69508
Thodoris Sotiropoulos <theosotr@aueb.gr>
2021-01-10 19:10:04 +0200

    Add two example bugs

---
+++ example_bugs/django_bug/Schemadb.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS "t1";
+CREATE TABLE "t1" (
+"id" integer,
+PRIMARY KEY (id)
+);
+INSERT INTO "t1" ("id") VALUES (1);
+
+DROP TABLE IF EXISTS "t2";
+CREATE TABLE "t2" (
+"id" integer,
+PRIMARY KEY (id)
+);
+INSERT INTO "t2" ("id") VALUES (1);
+
+DROP TABLE IF EXISTS "t3";
+CREATE TABLE "t3" (
+"id" integer,
+PRIMARY KEY (id)
+);
+INSERT INTO "t3" ("id") VALUES (1);
