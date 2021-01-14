c1de8c79e41a374326a370db7bede823ffcb71c3
Stefanos Chaliasos <stefanoshaliassos@gmail.com>
2021-01-15 01:12:13 +0200

    Use mysql 8

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -7,7 +7,17 @@ echo "Initialize Backends"
 
 source $HOME/.env/bin/activate
 nohup sudo /opt/mssql/bin/sqlservr start &
-sudo /etc/init.d/mysql start
+sudo /usr/bin/mysqld_safe >/dev/null 2>&1 &
+timeout=30
+while ! /usr/bin/mysqladmin -u root status >/dev/null 2>&1
+do
+  timeout=$(($timeout - 1))
+  if [ $timeout -eq 0 ]; then
+    echo -e "\nCould not connect to database server. Aborting..."
+    exit 1
+  fi
+  sleep 1
+done
 sudo /etc/init.d/postgresql start
 
 
