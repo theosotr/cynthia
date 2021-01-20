c388ba575fb38965f8cecc36f0f6eac7033bba92
Thodoris Sotiropoulos <theosotr@aueb.gr>
2021-01-20 19:08:01 +0200

    Retry to connect to Postgres

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -9,8 +9,16 @@ source $HOME/.env/bin/activate
 nohup sudo /opt/mssql/bin/sqlservr start &
 sudo /etc/init.d/mysql start
 sudo mysql --execute "SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));"
-sudo /etc/init.d/postgresql start
 
+timeout=30
+while ! sudo /etc/init.d/postgresql start 2>&1
+do
+  timeout=$(($timeout - 1))
+  if [ $timeout -eq 0 ]; then
+    echo -e "\nCould not connect to PostgreSQL. Aborting..."
+    exit 1
+  fi
+done
 
 source $HOME/.env/bin/activate
 if [ $# -eq 0  ]; then
